9b177942629d89208ceba4192c02b0dddb117c65
python128 <tejasgudgunti@gmail.com>
2021-06-12 09:43:18 +0530

    Added colors

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 /* 
-Problem: Easy to use logger. 
+Item: Easy to use logger.
 Maintainer: github.com/python128
 Target Users: CLI users
 Target UI: Linux, any CLI UI
 */
 
-
 use std::io::Write;
 use std::io::Read;
 use std::fs::OpenOptions;
 use chrono::Utc;
+use colored::*;
 
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
 	let action = &args[1];
 	let item = &args[2];
+	let log_file = "log";
 
 	if action == "create" {
 		create_file(item.to_string());
 	} else if action == "view" {
 		print_file(item.to_string());
-	} else {
-		append_to_file(action.to_string(), item.to_string());
+	} else if action == "log" {
+		append_to_file(log_file.to_string(), item.to_string());
 	}
 }
 
 fn create_file(file_name: String) {
-	std::fs::File::create(&file_name).expect("Creation failed");
-	println!("\n\nCreated file named {}\n", file_name);
+	std::fs::File::create(&file_name).expect(&"\nCreation failed".red());
+	println!("\n\nCreated file named {}\n", file_name.blue());
 }
 
 fn print_file(file_name: String) {
@@ -36,21 +37,26 @@ fn print_file(file_name: String) {
 	file
 		.read_to_string(&mut contents)
 		.unwrap();
-	println!("\n\nContents of file {}: \n\n{}\n", file_name, contents);
+	println!("\n\nContents of file {}: \n\n{}\n", file_name.blue().italic(), contents.truecolor(200,200,200));
 }
 
 fn append_to_file(file_name: String, text: String) {
 	let mut file = OpenOptions::new()
 		.append(true)
 		.open(&file_name)
-		.expect("\nCannot open file.");
+		.expect(&"\nCannot Open File.".red());
 
 	let now = Utc::now().to_string();
 	let ftext = "\n".to_owned() + &now + ": " + &text;
 
 	file
 		.write_all(ftext.as_bytes())
-		.expect("\nWrite failed.");
+		.expect(&"\nWrite Failed".red());
 
-	println!("\n\nSuccessfully appended to file {}\n", file_name);
+	println!("\n\n{}: {}\n", "Successfully appended to file" , file_name.green().bold());
+}
+
+struct Tag {
+	name: String,
+	imp: u32 //importance
 }
